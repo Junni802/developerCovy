507a19c03621668e3dfc0f44db6f59fab69e349d
covy <junni802@naver.com>
2025-07-04 16:54:24 +0900

    feat: add

---
+++ src/main/java/covy/developercovy/DeveloperCovyApplication.java
@@ -0,0 +1,13 @@
+package covy.developercovy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeveloperCovyApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DeveloperCovyApplication.class, args);
+  }
+
+}
---
+++ src/test/java/covy/developercovy/DeveloperCovyApplicationTests.java
@@ -0,0 +1,13 @@
+package covy.developercovy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeveloperCovyApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
